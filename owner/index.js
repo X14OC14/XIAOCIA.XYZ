7d4be0e855c99010acc4cfd8cc0830464325caf7
X I Λ O C I Λ <xiaocia@outlook.co.id>
2024-12-03 08:26:01 +0700

    Update

---
+++ owner/index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>owner</title>
+    <meta http-equiv="refresh" content="0; url=https://wa.me/6283145799197">
+</head>
+<body>
+    <p>If you are not redirected automatically, follow this <a href="https://wa.me/6283145799197">link</a>.</p>
+</body>
+</html>
